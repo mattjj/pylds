fixing bugs in missing data resampling. updating normalizing constants. cleaning up examples

examples/missing_data.py
@@ -22,8 +22,9 @@ A = 0.99*np.array([[np.cos(np.pi/24), -np.sin(np.pi/24)],
                    [np.sin(np.pi/24),  np.cos(np.pi/24)]])
 sigma_states = 0.01*np.eye(2)
 
-C = np.array([[10.,0.]])
-sigma_obs = 0.01*np.eye(D_obs)
+# C = np.array([[10.,0.]])
+C = np.random.randn(D_obs, D_latent)
+sigma_obs = 0.1*np.eye(D_obs)
 
 
 ###################
@@ -34,15 +35,20 @@ truemodel = LDS(
     dynamics_distn=AutoRegression(A=A,sigma=sigma_states),
     emission_distn=DiagonalRegression(D_obs, D_latent, A=C, sigmasq=np.diag(sigma_obs)))
 
+truemodel.mu_init = mu_init
+truemodel.sigma_init = sigma_init
+
 T = 2000
 data, stateseq = truemodel.generate(T)
 
 # Mask off a chunk of data
 mask = np.ones_like(data, dtype=bool)
-mask_start = 900
-mask_stop = 1200
+mask_start = 1000
+mask_stop = 1100
 mask_len = mask_stop - mask_start
 mask[mask_start:mask_stop] = False
+
+true_ll = truemodel.log_likelihood()
 
 ###############
 #  make model #
@@ -53,33 +59,61 @@ model = LDS(
             S_0=D_latent*np.eye(D_latent),
             M_0=np.zeros((D_latent, D_latent)),
             K_0=D_latent*np.eye(D_latent)),
-    emission_distn=DiagonalRegression(D_obs, D_latent))
+    emission_distn=DiagonalRegression(D_obs, D_latent, alpha_0=2.0, beta_0=1.0))
 model.add_data(data=data, mask=mask)
+
+print("True LL: ", truemodel.log_likelihood(data, mask))
+print("Init LL: ", model.log_likelihood(data, mask))
+
 
 ###############
 #  fit model  #
 ###############
 N_samples = 500
-def update(model):
-    # model.resample_model()
-    # model.EM_step()
-    model.meanfield_coordinate_descent_step()
+sigma_obs_smpls = []
+def gibbs_update(model):
+    model.resample_model()
+    sigma_obs_smpls.append(model.sigma_obs_flat[0])
     return model.log_likelihood()
 
-# Initialize with a few Gibbs iterations
-[model.resample_model() for _ in progprint_xrange(10)]
-lls = [update(model) for _ in progprint_xrange(N_samples)]
+def em_update(model):
+    model.EM_step()
+    sigma_obs_smpls.append(model.sigma_obs_flat[0])
+    return model.log_likelihood()
 
-plt.figure(figsize=(3,4))
-plt.plot(lls)
+def meanfield_update(model):
+    model.meanfield_coordinate_descent_step()
+    sigma_obs_smpls.append(model.emission_distn.mf_beta / model.emission_distn.mf_alpha)
+    return model.log_likelihood()
+
+# Gibbs
+# lls = [gibbs_update(model) for _ in progprint_xrange(N_samples)]
+
+# EM -- initialized with a few Gibbs iterations
+[model.resample_model() for _ in progprint_xrange(100)]
+lls = [em_update(model) for _ in progprint_xrange(N_samples)]
+
+# Mean field
+# lls = [meanfield_update(model) for _ in progprint_xrange(N_samples)]
+
+plt.figure()
+plt.plot(sigma_obs_smpls)
+plt.xlabel("iteration")
+plt.ylabel("sigma_obs")
+
+plt.figure()
+plt.plot(lls,'-b')
+plt.plot([0,N_samples], truemodel.log_likelihood(data, mask) * np.ones(2), '-k')
 plt.xlabel('iteration')
 plt.ylabel('log likelihood')
+
 
 
 ################
 #  smoothing   #
 ################
 smoothed_obs = model.smooth(data, mask=mask)
+sample_predictive_obs = model.states_list[0].stateseq.dot(model.C.T)
 
 plt.figure()
 given_data = data.copy()
@@ -90,6 +124,7 @@ masked_data[mask] = np.nan
 plt.plot(given_data, 'k', label="observed")
 plt.plot(masked_data, ':k', label="masked")
 plt.plot(smoothed_obs, 'b', label="smoothed")
+plt.plot(sample_predictive_obs, ':b', label="sample")
 plt.xlabel('time index')
 plt.ylabel('observation')
 plt.xlim([max(mask_start-mask_len, 0), min(mask_stop+mask_len, T)])
@@ -99,20 +134,19 @@ plt.legend(loc="upper right")
 ################
 #  predicting  #
 ################
-Nseed = 1700
-Npredict = 100
-prediction_seed = data[:Nseed]
-
-preds = model.sample_predictions(
-    prediction_seed, Npredict, obs_noise=False)
-
-plt.figure()
-plt.plot(given_data, 'k', label="observed")
-plt.plot(masked_data, ':k', label="masked")
-plt.plot(Nseed + np.arange(Npredict), preds, 'b')
-plt.xlabel('time index')
-plt.ylabel('prediction')
-# plt.xlim(1800,2000)
+# Nseed = 1700
+# Npredict = 100
+# prediction_seed = data[:Nseed]
+#
+# preds = model.sample_predictions(
+#     prediction_seed, Npredict, obs_noise=False)
+#
+# plt.figure()
+# plt.plot(given_data, 'k', label="observed")
+# plt.plot(masked_data, ':k', label="masked")
+# plt.plot(Nseed + np.arange(Npredict), preds, 'b')
+# plt.xlabel('time index')
+# plt.ylabel('prediction')
 
 plt.show()
 

pylds/models.py
@@ -35,11 +35,11 @@ class _LDSBase(Model):
 
         return self
 
-    def log_likelihood(self,data=None):
+    def log_likelihood(self,data=None,mask=None):
         if data is not None:
             assert isinstance(data,(list,np.ndarray))
             if isinstance(data,np.ndarray):
-                self.add_data(data=data,generate=False)
+                self.add_data(data=data,mask=mask,generate=False)
                 return self.states_list.pop().log_likelihood()
             else:
                 return sum(self.log_likelihood(d) for d in data)
@@ -161,6 +161,12 @@ class _LDSBase(Model):
     def is_stable(self):
         return np.max(np.abs(np.linalg.eigvals(self.dynamics_distn.A))) < 1.
 
+    @property
+    def has_missing_data(self):
+        m = [isinstance(s, LDSStatesMissingData) for s in self.states_list]
+        assert all(m) or (not any(m))
+        return all(m)
+
 
 class _LDSGibbsSampling(_LDSBase, ModelGibbsSampling):
     def resample_model(self):
@@ -180,9 +186,13 @@ class _LDSGibbsSampling(_LDSBase, ModelGibbsSampling):
             [s.strided_stateseq for s in self.states_list])
 
     def resample_emission_distn(self):
-        self.emission_distn.resample(
-            [np.hstack((s.stateseq,s.data)) for s in self.states_list])
-
+        xys = [np.hstack((s.stateseq, s.data)) for s in self.states_list]
+        # Provide a mask if necessary
+        if self.has_missing_data:
+            masks = [s.mask for s in self.states_list]
+            self.emission_distn.resample(data=xys, mask=masks)
+        else:
+            self.emission_distn.resample(data=xys)
 
 class _LDSMeanField(_LDSBase, ModelMeanField):
     def meanfield_coordinate_descent_step(self):