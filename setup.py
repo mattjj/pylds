f1d09a429cebb198d49796d3fe3ac392438afbab
Matthew Johnson <mattjj@csail.mit.edu>
2016-08-31 11:53:41 -0400

    setup call should pass ext_modules, not call cythonize

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
     packages=['pylds'],
     install_requires=[
         'numpy>=1.9.3', 'scipy>=0.16', 'matplotlib', 'pybasicbayes'],
-    ext_modules=cythonize('pylds/**/*.pyx'),
+    ext_modules=ext_modules,
     classifiers=[
         'Intended Audience :: Science/Research',
         'Programming Language :: Python',
